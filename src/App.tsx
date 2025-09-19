a9b51d1be1f1c62dfa8d64b7a261725919ba4dbc
suman-3 <mondalsumanrock20@gmail.com>
2025-09-19 20:51:21 +0530

    feat: add @vercel/analytics integration and update package dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import AppLayout from "./components/layouts/AppLayout";
 import Explorer from "./components/modules/Explorer/Explorer";
 import EditorView from "./components/modules/EditorArea/EditorView";
+import { Analytics } from '@vercel/analytics/react';
+
 
 function App() {
   return (
     <AppLayout>
       <Explorer />
       <EditorView />
+      <Analytics />
     </AppLayout>
   );
 }
